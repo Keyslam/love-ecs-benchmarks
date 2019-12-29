b959f3ac33f98664c8e24a773ccfadc2b22b4427
Andrew Minnich <aminnich3@gmail.com>
2019-12-29 16:54:06 -0500

    update to the newest version of nata

--- source/nata/init.lua
+++ source/nata/init.lua
@@ -1,76 +1,68 @@
-local function createEntity()
-	return {
-		x = 0,
-		y = 0,
-		vx = love.math.random(10, 30),
-		vy = love.math.random(10, 30),
-		sprite = true,
-	}
+local physics = {}
+
+function physics:update(dt)
+	for _, e in ipairs(self.pool.groups.velocity.entities) do
+		e.x = e.x + e.vx * dt
+		e.y = e.y + e.vy * dt
+		if e.x > love.graphics.getWidth() or e.y > love.graphics.getHeight() then
+			if love.math.random() < .4 then
+				e.dead = true
+			else
+				e.x = 0
+				e.y = 0
+			end
+		end
+	end
 end
 
-local maxX, maxY = love.graphics.getWidth(), love.graphics.getHeight()
+local spriteRenderer = {}
 
-local physicsSystem = {
-	filter = {'x', 'y', 'vx', 'vy'},
+function spriteRenderer:draw()
+	if not enableDrawing then return end
+	for _, e in ipairs(self.pool.groups.sprite.entities) do
+		love.graphics.draw(testSprite, e.x, e.y)
+	end
+end
 
-	process = {
-		update = function(self, dt)
-			for _, e in ipairs(self.entities) do
-				e.x = e.x + e.vx * dt
-				e.y = e.y + e.vy * dt
-				if e.x > maxX or e.y > maxY then
-					if love.math.random() < .4 then
-						e.dead = true
-					else
-						e.x = 0
-						e.y = 0
-					end
-				end
-			end
-		end,
+local pool = require 'nata.nata'.new {
+	groups = {
+		velocity = {filter = {'x', 'y', 'vx', 'vy'}},
+		sprite = {filter = {'x', 'y', 'sprite'}},
+	},
+	systems = {
+		physics,
+		spriteRenderer
 	},
 }
 
-local spriteRendererSystem = {
-	filter = {'x', 'y', 'sprite'},
+local removeCondition = function(e) return e.dead end
 
-	process = {
-		draw = function(self)
-			for _, e in ipairs(self.entities) do
-				if enableDrawing then
-					love.graphics.draw(testSprite, e.x, e.y)
-				end
-			end
-		end,
-	}
+local nataDemo = {}
 
-}
-
-local entities = require 'nata.nata'.new {
-	physicsSystem,
-	spriteRendererSystem
-}
-
-local removeCheck = function(e) return e.dead end
-
-return {
-	update = function(dt)
-		for _ = 1, 100 do
-			if entityLimit and #entities.entities >= entityLimit then
-				break
-			end
-			entities:queue(createEntity())
-			entities:flush()
+function nataDemo.update(dt)
+	for _ = 1, 100 do
+		if entityLimit and #pool.entities > entityLimit then
+			break
 		end
-		entities:process('update', dt)
-		entities:remove(removeCheck)
-	end,
+		pool:queue {
+			x = 0,
+			y = 0,
+			vx = love.math.random(10, 30),
+			vy = love.math.random(10, 30),
+			sprite = true,
+		}
+	end
+	pool:flush()
+	pool:emit('update', dt)
+	pool:remove(removeCondition)
+end
 
-	draw = function()
-		entities:process 'draw'
-	end,
+function nataDemo.draw()
+	pool:emit 'draw'
+end
 
-	getNumEntities = function()
-		return #entities.entities
-	end,
-}
+function nataDemo.getNumEntities()
+	return #pool.entities
+end
+
+return nataDemo
--- source/nata/nata.lua
+++ source/nata/nata.lua
@@ -5,7 +5,7 @@ local nata = {
 	_LICENSE = [[
 		MIT License
 
-		Copyright (c) 2018 Andrew Minnich
+		Copyright (c) 2019 Andrew Minnich
 
 		Permission is hereby granted, free of charge, to any person obtaining a copy
 		of this software and associated documentation files (the "Software"), to deal
@@ -27,173 +27,205 @@ local nata = {
 	]]
 }
 
-local System = {}
-System.__index = System
+local function removeByValue(t, v)
+	for i = #t, 1, -1 do
+		if t[i] == v then table.remove(t, i) end
+	end
+end
 
-function System:_shouldProcess(entity)
-	if type(self._definition.filter) == 'table' then
-		for _, component in ipairs(self._definition.filter) do
-			if not entity[component] then
-				return false
-			end
-		end
-		return true
-	elseif type(self._definition.filter) == 'function' then
-		return self._definition.filter(self, entity)
+local function entityHasKeys(entity, keys)
+	for _, key in ipairs(keys) do
+		if not entity[key] then return false end
 	end
 	return true
 end
 
-function System:_addEntity(entity)
-	if not self:_shouldProcess(entity) then
-		return false
+local function filterEntity(entity, filter)
+	if type(filter) == 'table' then
+		return entityHasKeys(entity, filter)
+	elseif type(filter) == 'function' then
+		return filter(entity)
 	end
-	table.insert(self.entities, entity)
-	self.hasEntity[entity] = true
-	if self._definition.sort then
-		self._sorted = false
-	end
+	return true
 end
-
-function System:_removeEntity(entity)
-	if not self.hasEntity[entity] then
-		return false
-	end
-	for i = #self.entities, 1, -1 do
-		if self.entities[i] == entity then
-			table.remove(self.entities, i)
-			break
-		end
-	end
-	self.hasEntity[entity] = false
-end
-
-function System:_sort()
-	if self._definition.sort and not self._sorted then
-		table.sort(self.entities, self._definition.sort)
-		self._sorted = true
-	end
-end
-
-function System:_process(name, ...)
-	self:_sort()
-	if self._definition.process and self._definition.process[name] then
-		self._definition.process[name](self, ...)
-	end
-end
-
-function System:_trigger(event, entity, ...)
-	if not self._definition.on then return false end
-	if not self._definition.on[event] then return false end
-	if not self.hasEntity[entity] then return false end
-	self:_sort()
-	self._definition.on[event](self, entity, ...)
-end
-
-function System:queue(...) self._pool:queue(...) end
-function System:process(...) self._pool:process(...) end
-function System:trigger(...) self._pool:trigger(...) end
-
-local function newSystem(pool, definition)
-	local system = setmetatable({
-		entities = {},
-		hasEntity = {},
-		_pool = pool,
-		_definition = definition,
-		_sorted = false,
-	}, System)
-	if system._definition.init then system._definition.init(system) end
-	return system
-end
-
-nata.oop = {
-	process = setmetatable({_f = {}}, {
-		__index = function(t, k)
-			if k == 'f' then
-				return rawget(t, k)
-			else
-				t._f[k] = t._f[k] or function(self, ...)
-					for _, entity in ipairs(self.entities) do
-						if type(entity[k]) == 'function' then
-							entity[k](entity, ...)
-						end
-					end
-				end
-				return t._f[k]
-			end
-		end,
-	}),
-	on = setmetatable({_f = {}}, {
-		__index = function(t, k)
-			if k == 'f' then
-				return rawget(t, k)
-			else
-				t._f[k] = t._f[k] or function(self, entity, ...)
-					if type(entity[k]) == 'function' then
-						entity[k](entity, ...)
-					end
-				end
-				return t._f[k]
-			end
-		end,
-	}),
-}
 
 local Pool = {}
 Pool.__index = Pool
 
-function Pool:trigger(event, entity, ...)
-	for _, system in ipairs(self._systems) do
-		system:_trigger(event, entity, ...)
+function Pool:_init(options, ...)
+	options = options or {}
+	self._queue = {}
+	self.entities = {}
+	self.hasEntity = {}
+	self.groups = {}
+	self._systems = {}
+	self._events = {}
+	self.data = options.data or {}
+	local groups = options.groups or {}
+	local systems = options.systems or {nata.oop()}
+	for groupName, groupOptions in pairs(groups) do
+		self.groups[groupName] = {
+			filter = groupOptions.filter,
+			sort = groupOptions.sort,
+			entities = {},
+			hasEntity = {},
+		}
 	end
+	for _, systemDefinition in ipairs(systems) do
+		local system = setmetatable({
+			pool = self,
+		}, {__index = systemDefinition})
+		table.insert(self._systems, system)
+	end
+	self:emit('init', ...)
 end
 
-function Pool:process(name, ...)
-	for _, system in ipairs(self._systems) do
-		system:_process(name, ...)
-	end
+function Pool:_addToGroup(groupName, entity)
+	local group = self.groups[groupName]
+	table.insert(group.entities, entity)
+	group.hasEntity[entity] = true
+	self:emit('addToGroup', groupName, entity)
 end
 
-function Pool:queue(entity, ...)
-	table.insert(self._queue, {entity, {...}})
+function Pool:_removeFromGroup(groupName, entity)
+	local group = self.groups[groupName]
+	removeByValue(group.entities, entity)
+	group.hasEntity[entity] = nil
+	self:emit('removeFromGroup', groupName, entity)
+end
+
+function Pool:queue(entity)
+	table.insert(self._queue, entity)
 	return entity
 end
 
 function Pool:flush()
-	for i, v in ipairs(self._queue) do
-		local entity, args = v[1], v[2]
-		table.insert(self.entities, entity)
-		for _, system in ipairs(self._systems) do
-			system:_addEntity(entity)
+	for i = 1, #self._queue do
+		local entity = self._queue[i]
+		-- check if the entity belongs in each group and
+		-- add it to/remove it from the group as needed
+		for groupName, group in pairs(self.groups) do
+			if filterEntity(entity, group.filter) then
+				if not group.hasEntity[entity] then
+					self:_addToGroup(groupName, entity)
+				end
+				if group.sort then group._needsResort = true end
+			elseif group.hasEntity[entity] then
+				self:_removeFromGroup(groupName, entity)
+			end
 		end
-		self:trigger('add', entity, unpack(args))
+		-- add the entity to the pool if it hasn't been added already
+		if not self.hasEntity[entity] then
+			table.insert(self.entities, entity)
+			self.hasEntity[entity] = true
+			self:emit('add', entity)
+		end
 		self._queue[i] = nil
+	end
+	-- re-sort groups
+	for _, group in pairs(self.groups) do
+		if group._needsResort then
+			table.sort(group.entities, group.sort)
+			group._needsResort = nil
+		end
 	end
 end
 
-function Pool:remove(f, ...)
+function Pool:remove(f)
 	for i = #self.entities, 1, -1 do
 		local entity = self.entities[i]
 		if f(entity) then
-			self:trigger('remove', entity, ...)
-			for _, system in ipairs(self._systems) do
-				system:_removeEntity(entity)
+			self:emit('remove', entity)
+			for groupName, group in pairs(self.groups) do
+				if group.hasEntity[entity] then
+					self:_removeFromGroup(groupName, entity)
+				end
 			end
 			table.remove(self.entities, i)
+			self.hasEntity[entity] = nil
 		end
 	end
 end
 
-function nata.new(systems)
-	systems = systems or {nata.oop}
-	local pool = setmetatable({
-		entities = {},
-		_systems = {},
-		_queue = {},
-	}, Pool)
-	for _, system in ipairs(systems) do
-		table.insert(pool._systems, newSystem(pool, system))
+function Pool:on(event, f)
+	self._events[event] = self._events[event] or {}
+	table.insert(self._events[event], f)
+	return f
+end
+
+function Pool:off(event, f)
+	if self._events[event] then
+		removeByValue(self._events[event], f)
 	end
+end
+
+function Pool:emit(event, ...)
+	for _, system in ipairs(self._systems) do
+		if type(system[event]) == 'function' then
+			system[event](system, ...)
+		end
+	end
+	if self._events[event] then
+		for _, f in ipairs(self._events[event]) do
+			f(...)
+		end
+	end
+end
+
+function Pool:getSystem(systemDefinition)
+	for _, system in ipairs(self._systems) do
+		if getmetatable(system).__index == systemDefinition then
+			return system
+		end
+	end
+end
+
+function nata.oop(options)
+	local group = options and options.group
+	local include, exclude
+	if options and options.include then
+		include = {}
+		for _, event in ipairs(options.include) do
+			include[event] = true
+		end
+	end
+	if options and options.exclude then
+		exclude = {}
+		for _, event in ipairs(options.exclude) do
+			exclude[event] = true
+		end
+	end
+	return setmetatable({_cache = {}}, {
+		__index = function(t, event)
+			t._cache[event] = t._cache[event] or function(self, ...)
+				local shouldCallEvent = true
+				if include and not include[event] then shouldCallEvent = false end
+				if exclude and exclude[event] then shouldCallEvent = false end
+				if shouldCallEvent then
+					local entities
+					-- not using ternary here because if the group doesn't exist,
+					-- i'd rather it cause an error than just silently falling back
+					-- to the main entity pool
+					if group then
+						entities = self.pool.groups[group].entities
+					else
+						entities = self.pool.entities
+					end
+					for _, entity in ipairs(entities) do
+						if type(entity[event]) == 'function' then
+							entity[event](entity, ...)
+						end
+					end
+				end
+			end
+			return t._cache[event]
+		end
+	})
+end
+
+function nata.new(...)
+	local pool = setmetatable({}, Pool)
+	pool:_init(...)
 	return pool
 end
 
